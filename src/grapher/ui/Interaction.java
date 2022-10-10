cf913676a23dc8131bc3829001a96a8ae03d0f12
SOLER Lilian <soler.lilian64@gmail.com>
2022-10-10 15:40:19 +0200

    Squelette Interaction

---
+++ src/grapher/ui/Interaction.java
@@ -0,0 +1,79 @@
+package grapher.ui;
+
+import java.awt.event.*;
+
+public class Interaction implements MouseListener, MouseMotionListener, MouseWheelListener {
+	private State state;
+	@Override
+	public void mouseClicked(MouseEvent mouseEvent) {
+		switch(mouseEvent.getButton()) {
+		case MouseEvent.BUTTON1:
+			state = State.ClickedLeft;
+			break;
+		case MouseEvent.BUTTON2:
+			state = State.ClickedWheel;
+			break;
+		case MouseEvent.BUTTON3:
+			state = State.ClickedRight;
+			break;
+		}
+		System.out.println("mouseClicked: " + state);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent mouseEvent) {
+		switch(mouseEvent.getButton()) {
+		case MouseEvent.BUTTON1:
+			state = State.PressedLeft;
+			break;
+		case MouseEvent.BUTTON2:
+			state = State.PressedWheel;
+			break;
+		case MouseEvent.BUTTON3:
+			state = State.PressedRight;
+			break;
+		}
+		System.out.println("mousePressed: " + state);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent mouseEvent) {
+		state = State.Default;
+		System.out.println("mouseReleased: " + state);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent mouseEvent) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent mouseEvent) {
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent mouseEvent) {
+		switch(mouseEvent.getButton()) {
+		case MouseEvent.BUTTON1:
+			state = State.DraggedLeft;
+			break;
+		case MouseEvent.BUTTON2:
+			state = State.DraggedWheel;
+			break;
+		}
+		System.out.println("mouseDragged: " + state);
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent mouseEvent) {
+		System.out.println("mouseMoved: " + state);
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent mouseWheelEvent) {
+		state = State.DraggedWheel;
+		System.out.println("mouseWheelMoved: " + state);
+		System.out.println("mouseWheelMoved: " + mouseWheelEvent.getWheelRotation());
+	}
+}
---
+++ src/grapher/ui/State.java
@@ -0,0 +1,5 @@
+package grapher.ui;
+
+public enum State {
+	Default, PressedLeft, PressedRight, PressedWheel, DraggedLeft, DraggedRight, DraggedWheel, ClickedLeft, ClickedRight, ClickedWheel
+}
